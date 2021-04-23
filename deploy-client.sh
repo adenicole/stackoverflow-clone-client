43996c8958ba9473c0da25b8f4c45774df8af580
adenicole <dedoyinnicole@gmail.com>
2021-04-23 20:58:48 +0100

    Add sudo to docker commands

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-RUNNING_CLIENT_CONTAINERS=$(docker ps | grep stackoverflow-client | awk '{print $1}')
-ALL_CLIENT_CONTAINERS=$(docker ps -a | grep stackoverflow-client | awk '{print $1}')
+RUNNING_CLIENT_CONTAINERS=$(sudo docker ps | grep stackoverflow-client | awk '{print $1}')
+ALL_CLIENT_CONTAINERS=$(sudo docker ps -a | grep stackoverflow-client | awk '{print $1}')
 
 # Stop and remove old containers
-docker stop "${RUNNING_CLIENT_CONTAINERS}"
-docker container rm "${ALL_CLIENT_CONTAINERS}"
+sudo docker stop "${RUNNING_CLIENT_CONTAINERS}"
+sudo docker container rm "${ALL_CLIENT_CONTAINERS}"
 
 # Build API app's image
-docker build -t adenicole/stackoverflow-client .
+sudo docker build -t adenicole/stackoverflow-client .
 
 # Deploy the API app in a container and map exposed port to localhost:80
-docker run -d -i -p 80:3000 --network stackoverflow-net --name stackoverflow-client adenicole/stackoverflow-client
+sudo docker run -d -i -p 80:3000 --network stackoverflow-net --name stackoverflow-client adenicole/stackoverflow-client
